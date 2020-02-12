7e3cc90d25fa2c67fad9a4a2e84fa68be43c20c3
HENRY BENAYE <2106747@talnet.nl>
2020-02-12 14:15:53 +0100

    Uitwerking van de exercise

--- form.php
+++ form.php
@@ -1,18 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>form</title>
 </head>
+
 <body>
     <form action="form.php" method="get">
-        <input type="text" placeholder="Voer je e-mailadres in" name="mail" >
+        <input type="text" placeholder="Voer je e-mailadres in" name="mail">
         <input type="submit" value="Verstuur">
     </form>
-    
-    
+
+
 </body>
+
 </html>
 <?php
 
